f0a90d31acead5f00b65176bb5d9a71639fbfe47
carolinedilone <caroline.dilone@gmail.com>
2017-04-26 21:59:03 -0400

    Add files via upload

---
+++ aboutme_main.js
@@ -0,0 +1,14 @@
+$(document).click(function() {
+
+	$(".i-love").click(function(){
+		$("ol.very-fast").hide(60);
+		$("ol.normal").hide();
+		$("ol.fast").hide("fast");
+		$("ol.slow").hide("slow");
+		$("ol.very-slow").hide(1500);
+
+	}); // hide 
+	
+
+	}); //ends doc 
+
